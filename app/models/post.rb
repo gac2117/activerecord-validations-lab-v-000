2d60febb6661fda8d4523605886fcb78e7653fc2
Grace Choi <oiograceoio@gmail.com>
2019-04-24 19:19:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {in: 10..300}
   validates :summary, length: {maximum: 10}
+  validates :category, inclusion: { in:%w(Fiction)}
 end
